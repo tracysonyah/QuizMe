26dfd2f6193dc7b014088a7c36538b7f5283c97c
Tracy Sonyah <sonyahtracy@gmail.com>
2023-06-14 14:37:29 +0000

    not done yet

---
+++ script.js
@@ -0,0 +1,35 @@
+const questions = [
+    {
+        question: "What is Ruby, in the tech world?",
+        Answer: [
+            {text: "A shiny gem", correct: false},
+            {text: "A programming language", correct: true},
+            {text: "Your girlfriend's name", correct: false},
+            {text: "A text editor", correct: false}
+        ]
+    },
+
+    {
+        question: "What is the symbol used to define a single-line comment in Ruby?",
+        Answer: [
+            {text: "#", correct: true},
+            {text: "@", correct: false},
+            {text: "$", correct: false},
+            {text: "&!", correct: false}
+        ]
+    },
+
+    {
+        question: "What does the 'puts' method do in Ruby?",
+        Answer: [
+            {text: "Adds a newline character after an input", correct: false},
+            {text: "Adds a new sentence", correct: false},
+            {text: "Substract a new line", correct: false},
+            {text: "Adds a newline after the output", correct: true}
+        ]
+    },
+
+    {
+        question: ""
+    }
+]
